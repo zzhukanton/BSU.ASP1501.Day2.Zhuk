cdfc5857243dcb8b34d7eee5b4ed8a783863eaf7
zzhukanton <zzhukanton@gmail.com>
2015-10-17 03:35:26 +0200

    Task 4 first try

---
+++ Task4.NUnitTests/GCDTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task4;
+
+namespace Task4.NUnitTests
+{
+    [TestFixture]
+    public class GCDTests
+    {
+        [TestCase((long)0, 5, 125, Result = 5)]
+        [TestCase((long)0, 0, 0, ExpectedException = typeof(ArgumentException))]
+        [TestCase((long)0, 3, 7, Result = 1)]
+        [TestCase((long)0, -17, 34, Result = 17)]
+        [TestCase((long)0, 30, 0, Result = 30)]
+        public int EuclidAlgorithm_TwoNumbers(out long time, int first, int second)
+        {
+            time = 0;
+
+            int result = GCD.EuclidAlgorithm(out time, first, second);
+            return result;
+        }
+
+        [TestCase((long)0, 5, 125, Result = 5)]
+        [TestCase((long)0, 5, ExpectedException = typeof(ArgumentException))]
+        [TestCase((long)0, 5, 125, 625, 400, Result = 5)]
+        [TestCase((long)0, -5, 20, 60, Result = 5)]
+        [TestCase((long)0, new int[] {}, ExpectedException = typeof(ArgumentException))]
+        public int EuclidAlgorithm_ManyNumbers(out long time, params int[] numbers)
+        {
+            time = 0;
+
+            int result = GCD.EuclidAlgorithm(out time, numbers);
+            return result;
+        }
+
+        [TestCase((long)0, 5, 125, Result = 5)]
+        [TestCase((long)0, 0, 125, Result = 125)]
+        [TestCase((long)0, -5, 125, Result = 5)]
+        [TestCase((long)0, 0, 0, ExpectedException = typeof(ArgumentException))]
+        [TestCase((long)0, 5, 123, Result = 1)]
+        public int SteinAlgorithm_TwoNumbers(out long time, int first, int second)
+        {
+            time = 0;
+
+            int result = GCD.SteinAlgorithm(out time, first, second);
+            return result;
+        }
+
+        [TestCase((long)0, 5, 125, Result = 5)]
+        [TestCase((long)0, 5, ExpectedException = typeof(ArgumentException))]
+        [TestCase((long)0, 5, 125, 625, 400, Result = 5)]
+        [TestCase((long)0, -5, 20, 60, Result = 5)]
+        [TestCase((long)0, new int[] { }, ExpectedException = typeof(ArgumentException))]
+        public int SteinAlgorithm_ManyNumbers(out long time, params int[] numbers)
+        {
+            time = 0;
+
+            int result = GCD.SteinAlgorithm(out time, numbers);
+            return result;
+        }
+    }
+}
---
+++ Task4/GCD.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Task4
+{
+    /// <summary>
+    /// Class for finding gcd
+    /// </summary>
+    public static class GCD
+    {
+        #region Euclid
+        /// <summary>
+        /// Euclid algorithm for two integers
+        /// </summary>
+        /// <param name="calculationTime">Calculation time for algorithm</param>
+        /// <param name="number1">First number</param>
+        /// <param name="number2">Second number</param>
+        /// <returns>GCD</returns>
+        public static int EuclidAlgorithm(out long calculationTime, int number1, int number2)
+        {
+            if (number1 == 0 && number2 == 0)
+                throw new ArgumentException("Numbers are both zero");
+
+            if (number1 < 0)
+                number1 = Math.Abs(number1);
+
+            if (number2 < 0)
+                number2 = Math.Abs(number2);
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            while (number2 != 0)
+                number2 = number1 % (number1 = number2);
+            timer.Stop();
+            calculationTime = timer.ElapsedTicks;
+
+            return number1;
+        }
+
+        /// <summary>
+        /// Euclid algorithm for finding gcd for multiple numbers
+        /// </summary>
+        /// <param name="wholeTime">For save time to calculate</param>
+        /// <param name="numbers">Numbers</param>
+        /// <returns>GCD</returns>
+        public static int EuclidAlgorithm(out long wholeTime, params int[] numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException("Array is null");
+
+            if (numbers.Length == 0 || numbers.Length == 1)
+                throw new ArgumentException("Less than two numbers");
+
+            int result = numbers[0];
+            long time = 0;
+            wholeTime = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (result == 1)
+                    break;
+                result = EuclidAlgorithm(out time, result, numbers[i + 1]);
+                wholeTime += time;
+            }
+
+            return result;
+        }
+        #endregion
+
+        #region Stein
+        /// <summary>
+        /// Stein algorithm for finding gcd
+        /// </summary>
+        /// <param name="time">Algorithm working time</param>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>GCD by Stein algorithm</returns>
+        public static int SteinAlgorithm(out long time, int a, int b)
+        {
+            if (a == 0 && b == 0)
+                throw new ArgumentException("Both numbers are zero");
+
+            time = 0;
+            Stopwatch timer = new Stopwatch();
+            
+            timer.Start();
+            int result = SteinCalculation(a, b);
+            timer.Stop();
+            time = timer.ElapsedTicks;
+            
+            return result;
+        }
+
+        /// <summary>
+        /// Stein algorithm for multiple integer numbers with time
+        /// </summary>
+        /// <param name="wholeTime">Algorithm working time</param>
+        /// <param name="numbers">Numbers for gcd</param>
+        /// <returns>GCD of many numbers</returns>
+        public static int SteinAlgorithm(out long wholeTime, params int[] numbers)
+        {
+            if (numbers.Length == 0 || numbers.Length == 1)
+                throw new ArgumentException("Less than two arguments");
+
+            int result = numbers[0];
+            long time = 0;
+            wholeTime = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (result == 1)
+                    break;
+                result = SteinAlgorithm(out time, result, numbers[i + 1]);
+                wholeTime += time;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Recursive Stein algorithm for gcd
+        /// </summary>
+        /// <param name="number1">First number</param>
+        /// <param name="number2">Second number</param>
+        /// <returns>GCD</returns>
+        private static int SteinCalculation(int number1, int number2)
+        {
+            if (number1 == 0)
+                return number2;
+
+            if (number2 == 0)
+                return number1;
+
+            if (number1 == number2)
+                return number1;
+
+            if (number1 == 1 || number2 == 1)
+                return 1;
+
+            if ((number1 % 2 == 0) && (number2 % 2 == 0))
+                return 2 * SteinCalculation(number1 / 2, number2 / 2);
+
+            if ((number1 % 2 == 0) && (number2 % 2 != 0))
+                return SteinCalculation(number1 / 2, number2);
+
+            if ((number1 % 2 != 0) && (number2 % 2 == 0))
+                return SteinCalculation(number1, number2 / 2);
+
+            return SteinCalculation(number2, Math.Abs(number1 - number2));
+        }
+        #endregion
+    }
+}
