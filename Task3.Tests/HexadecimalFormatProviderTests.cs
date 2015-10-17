2624ece2dfa1a3ea6ea3b673cb0cd7aeae22d09c
zzhukanton <zzhukanton@gmail.com>
2015-10-17 23:30:02 +0200

    Task 3 first try

---
+++ Task3.Tests/HexadecimalFormatProviderTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task3;
+
+namespace Task3.Tests
+{
+    [TestFixture]
+    public class HexadecimalFormatProviderTests
+    {
+        [TestCase("{0:Hex}", 196, Result = "c4")]
+        [TestCase("{0:E} in hexadecimal is {0:Hex}", 196, Result = "1,960000E+002 in hexadecimal is c4")]
+        [TestCase("{0}", 200, Result = "200")]
+        [TestCase("", 200, Result = "")]
+        [TestCase(null, 200, ExpectedException = typeof(ArgumentNullException))]
+        public string Format_ReturnHexadecimalNumber(string arg, int number)
+        {
+            string result;
+            IFormatProvider fmt = new HexadecimalFormatProvider();
+
+            result = string.Format(fmt, arg, number);
+
+            return result;
+        }
+    }
+}
---
+++ Task3/HexadecimalFormatProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    /// <summary>
+    /// Class for representation integer in hexadecimal notation
+    /// </summary>
+    public class HexadecimalFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        /// <summary>
+        /// Hexadecimal digits
+        /// </summary>
+        private string _digits16 = "0123456789abcdef";
+
+        /// <summary>
+        /// Allow using many format providers
+        /// </summary>
+        IFormatProvider _parent;
+
+
+        public HexadecimalFormatProvider() : this(CultureInfo.CurrentCulture) { }
+
+        public HexadecimalFormatProvider(IFormatProvider parent)
+        {
+            _parent = parent;
+        }
+
+        /// <summary>
+        /// IFormatProvider member for getting ICustomFormatter object 
+        /// </summary>
+        /// <param name="formatType">Format type</param>
+        /// <returns>ICustomFormatter object</returns>
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+                return this;
+            return null;
+        }
+
+        /// <summary>
+        /// Hexadecimal string representation of integer 
+        /// </summary>
+        /// <param name="format">String with format(s)</param>
+        /// <param name="arg">Integer number</param>
+        /// <param name="formatProvider">IFormatProvider object implements hexadecimal view</param>
+        /// <returns>String hexadecimal representation of integer</returns>
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (arg == null)
+                throw new ArgumentNullException("String is null");
+
+            if (format != "Hex" || arg.GetType() != typeof(Int32))
+                return string.Format(_parent, "{0:" + format + "}", arg);
+
+            int number = (Int32)arg;
+            StringBuilder result = new StringBuilder();
+
+            while (number != 0)
+            {
+                int digit10 = number % 16;
+                result.Append(_digits16[digit10]);
+                number /= 16;
+            }
+            char[] helpArray = result.ToString().ToCharArray();
+            Array.Reverse(helpArray);
+
+            return new string(helpArray);
+        }
+    }
+}
